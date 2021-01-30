854495b583af991b159fde244e1ade792080557b
Christian Schuff <christian.schuff@gmail.com>
2021-01-30 22:56:22 +0100

    chore: forgotten file

--- src/makstr-client/einheit.ts
+++ src/makstr-client/einheit.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { BetriebsStatusId } from './interfaces/BetriebsStatusId.enum';
-import { Energietraeger } from './interfaces/Energietraeger.enum';
-import { EinheitResponse } from './responses/einheit-response.interface';
+import { BetriebsStatus } from './interfaces/betrieb-status.enum';
+import { Energietraeger } from './interfaces/energietraeger.enum';
+import { MaStrResponse } from './responses/einheit-response.interface';
 
 export interface EinheitFilterParams {
-  'Betriebs-Status': BetriebsStatusId;
+  'Betriebs-Status': BetriebsStatus;
   Gemeindeschlüssel: string;
   Energieträger: Energietraeger;
 }
@@ -20,17 +20,17 @@ export class Einheit {
   private baseUrl =
     'https://www.marktstammdatenregister.de/MaStR/Einheit/EinheitJson/GetVerkleinerteOeffentlicheEinheitStromerzeugung';
 
-  async query(gemeindeschluessel: string): Promise<EinheitResponse> {
+  async query(gemeindeschluessel: string): Promise<MaStrResponse> {
     const filter =
       `filter=` +
-      `Betriebs-Status~eq~%27${BetriebsStatusId.IN_BETRIEB}%27~and~` +
+      `Betriebs-Status~eq~%27${BetriebsStatus.IN_BETRIEB}%27~and~` +
       `Gemeindeschl%C3%BCssel~eq~%27${gemeindeschluessel}%27~and~` +
       `Energietr%C3%A4ger~eq~%27${Energietraeger.SOLARE_STRAHLUNGSENERGIE}%27`;
 
     const url = `${this.baseUrl}?page=1&pageSize=0&${filter}`;
     console.log(url);
 
-    const response = await axios.get<EinheitResponse>(url);
+    const response = await axios.get<MaStrResponse>(url);
     console.log(response);
 
     return response.data;
