bbe28d1441b3e711c32de0c10ca355f1bd8515df
Navid Nikpour <navid@nikpour.com>
2021-02-07 03:04:45 +0100

    fixes port part 2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { App } from './app';
 import { CalculatorController } from './controllers/calculator.controller';
 
 const app = new App({
-  port: parseInt(process.env.PORT) || 3000,
+  port: parseInt(process.env.PORT!) || 3000,
   controllers: [new CalculatorController()],
 });
 
