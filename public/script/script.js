3e682604cfc31e9c0bd3d4db770954b7d00a50c7
Christian Schuff <christian.schuff@gmail.com>
2021-02-10 21:21:40 +0100

    feat: ui changes, service changes

--- public/script/script.js
+++ public/script/script.js
@@ -1,14 +1,21 @@
 function validateNumber(length) {
-    return function(event) {
-        var $this = $(this);
-        var value = $this.val();
-
+    return function(value) {
         var lengthInvalid = false;
         if (length && value.length !== length) {
             lengthInvalid = true;
         }
         
-        if (value.length && (lengthInvalid || isNaN(new Number(value)))) {
+        return !(value.length && (lengthInvalid || isNaN(new Number(value))));
+    }
+}
+
+function validateAndHighlight(length) {
+    var validate = validateNumber(length);
+    return function(event) {
+        var $this = $(this);
+        var value = $this.val();
+        
+        if (validate(value)) {
             $this.addClass('error');
         } else {
             $this.removeClass('error');
@@ -16,17 +23,27 @@ function validateNumber(length) {
     }
 }
 
+var validateSchluessel = validateNumber(8);
+var validateEinwohner = validateNumber();
+
 function onHashChanged() {
     if (location.hash.length > 1) {
         var vals = location.hash.split('#').pop().split('/');
-        var schluessel = $('#gemeindeschluessel').val(vals[0]);
-        var einwohner = $('#einwohnerzahl').val(vals[1]);
+        $('#gemeindeschluessel').val(vals[0]);
+        $('#einwohnerzahl').val(vals[1]);
+
+        resetResult();
+        var schluessel = $('#gemeindeschluessel').val();
+        var einwohner = $('#einwohnerzahl').val();
+
+        if (validateSchluessel(schluessel) && validateEinwohner(einwohner)) {
+            calculateData(schluessel, einwohner);
+        }
     }
 }
 
 $(document).ready(function () {
     $(window).on('hashchange', onHashChanged);
-    onHashChanged();
     
     $('#submit').on('mouseup', function (event) {
         var schluessel = $('#gemeindeschluessel').val();
@@ -38,96 +55,104 @@ $(document).ready(function () {
         }
     });
 
-    $('#gemeindeschluessel').on('keyup', validateNumber(8));
-    $('#einwohnerzahl').on('keyup', validateNumber());
+    $('#gemeindeschluessel').on('keyup', validateAndHighlight(8));
+    $('#einwohnerzahl').on('keyup', validateAndHighlight());
+
+    onHashChanged();
 });
+
 
 function calculateData(schluessel, einwohner) {
     var timeout;
     $.ajax('/api/watt-info/' + schluessel + '/' + einwohner, {
         beforeSend: function() {
-            $('#result').css({ visibility: 'hidden' });
-            $('#result .table').css({ visibility: 'hidden' });
-            timeout = window.setTimeout(function() {
-                $('#loading').show();
-            }, 100);
+            $('#loading').show();
         },
         success: function(data) {
-            // window.clearTimeout(timeout);
+            $('#loading').hide();
+            $('#result').css({ visibility: 'visible', display: 'none' });
+            $('#result').fadeIn(300);
+            $('#result .table').css({ visibility: 'visible', display: 'none' });
+            $('#result .table').delay( 1000 ).fadeIn( 300 );
             
-            window.setTimeout(function() {
-                $('#loading').hide();
-                $('#result').css({ visibility: 'visible', display: 'none' });
-                $('#result').fadeIn(300);
-                $('#result .table').css({ visibility: 'visible', display: 'none' });
-                $('#result .table').delay( 1000 ).fadeIn( 300 );
-                
-                displayResult(data);
-            }, 500);
+            displayResult(data);
         }
     })
 }
 
 function resetResult() {
+    $('#result').css({ visibility: 'hidden' });
+    $('#result .table').css({ visibility: 'hidden' });
+    
     $('#result h1').text('');
 
     $('#result .big .total span.value').text('0');
     $('#result .big .anlagen span.value').text('0');
     $('#result .big .perResident span.value').text('0');
-
-    // $('#result .end2020 .date').text(data.end2020.date);
-    $('#result .end2020 .total').text('');
-    $('#result .end2020 .anlagen').text('');
-    $('#result .end2020 .perResident').text('');
-
-    // $('#result .now .date').text(data.now.date);
-    $('#result .now .total').text('');
-    $('#result .now .anlagen').text('');
-    $('#result .now .perResident').text('');
-
-    $('#result .growth .total').text('');
-    $('#result .growth .anlagen').text('');
-    $('#result .growth .perResident').text('');
 }
 
+// {
+//     ort,
+//     netzbetreiberName,
+//     gemeindeSchluessel,
+//     start: {
+//         dateFrom: fromDate?.format('DD.MM.YYYY'),
+//         dateTo: toDate?.format('DD.MM.YYYY'),
+//         total: {
+//             kwp: total,
+//             anlagen: data.length,
+//             module: totalModule,
+//             wpPerResident: perResidentNow,
+//         },
+//         geprueft: {
+//             kwp: geprueft,
+//             anlagen: geprueftData.length,
+//             module: geprueftModule,
+//             wpPerResident: geprueftPerResidentNow,
+//         },
+//         inPruefung: {
+//             kwp: inPruefung,
+//             anlagen: inPruefungData.length,
+//             module: inPruefungModule,
+//             wpPerResident: inPruefungPerResidentNow,
+//         },
+//     },
+//     growth: [],
+//     secs: durationInSec,
+// }
 function displayResult(data) {
     $('#result h1').text(data.ort);
 
     window.setTimeout(function() {
-        animateInt($('#result .big .total span.value'), 0, data.end2020.total, 1000);
-        animateInt($('#result .big .anlagen span.value'), 0, data.end2020.anlagen, 1000);
-        animateDecimal($('#result .big .perResident span.value'), 0, data.end2020.perResident, 1000);
+        const startTotal = data.start.total;
+        animateInt($('#result .big.start .total span.value'), 0, startTotal.kwp, 1000);
+        animateInt($('#result .big.start .anlagen span.value'), 0, startTotal.anlagen, 1000);
+        animateDecimal($('#result .big.start .perResident span.value'), 0, startTotal.wpPerResident, 1000);
     }, 300);
 
-    $('#result .end2020 .total').text(formatInt(data.end2020.total) + ' kWp');
-    $('#result .end2020 .anlagen').text(formatInt(data.end2020.anlagen));
-    $('#result .end2020 .perResident').text(formatDecimal(data.end2020.perResident) + ' Wp/Einwohner');
-
-    // $('#result .end2020 .date').text(data.end2020.date);
-    $('#result .end2020 .total').text(formatInt(data.end2020.total) + ' kWp');
-    $('#result .end2020 .anlagen').text(formatInt(data.end2020.anlagen));
-    $('#result .end2020 .perResident').text(formatDecimal(data.end2020.perResident) + ' Wp/Einwohner');
-
-    // $('#result .now .date').text(data.now.date);
-    $('#result .now .total').text(formatInt(data.now.total) + ' kWp');
-    $('#result .now .anlagen').text(formatInt(data.now.anlagen));
-    $('#result .now .perResident').text(formatDecimal(data.now.perResident) + ' Wp/Einwohner');
-
-    $('#result .growth .total').text(formatInt(data.now.total - data.end2020.total) + ' kWp');
-    $('#result .growth .anlagen').text(formatInt(data.now.anlagen - data.end2020.anlagen));
-    $('#result .growth .perResident').text(formatDecimal(data.now.perResident - data.end2020.perResident) + ' Wp/Einwohner');
+    const geprueft = data.start.geprueft;
+    $('#result .big.checked .total span.value').text(formatInt(geprueft.kwp));
+    $('#result .big.checked .anlagen span.value').text(formatInt(geprueft.anlagen));
+    $('#result .big.checked .perResident span.value').text(formatDecimal(geprueft.wpPerResident));
+    
+    const inPruefung = data.start.inPruefung;
+    $('#result .big.unchecked .total span.value').text(formatInt(inPruefung.kwp));
+    $('#result .big.unchecked .anlagen span.value').text(formatInt(inPruefung.anlagen));
+    $('#result .big.unchecked .perResident span.value').text(formatDecimal(inPruefung.wpPerResident));
 }
+
+
 
 function formatInt(val) {
     return new Number(val).toLocaleString(
-        undefined, // --> browser locale
+        'de', // --> browser locale
         { minimumFractionDigits: 0 }
       );
 }
 
 function formatDecimal(val) {
     return new Number(val).toLocaleString(
-        undefined, // --> browser locale
+        'de', // --> browser locale
         { minimumFractionDigits: 2, maximumFractionDigits: 2 }
       );
 }
@@ -161,3 +186,10 @@ function animateDecimal($el, start, end, duration) {
     };
     window.requestAnimationFrame(step);
 }
+
+function textToClipboard(el) {
+    var textToCopy = $(el).val()
+    textToCopy.select();
+    document.execCommand('copy');
+}
+
--- src/services/calculator-service.ts
+++ src/services/calculator-service.ts
@@ -1,4 +1,5 @@
 import { plainToClass } from 'class-transformer';
+import { clone } from 'lodash';
 import moment from 'moment';
 import { inspect } from 'util';
 import {
@@ -14,8 +15,8 @@ export class CalculatorService {
   constructor() {
     this.makStrClient = new MakstrClient();
   }
-  async calculate(gemeindeSchluessel: string, einwohnerZahl: number) {
-    const start = process.hrtime();
+
+  async requestData(gemeindeSchluessel: string) {
     const mainQuery = {
       Gemeindeschlüssel: `'${gemeindeSchluessel}'`,
       Energieträger: `'${Energietraeger.SOLARE_STRAHLUNGSENERGIE}'`,
@@ -45,12 +46,60 @@ export class CalculatorService {
       temporarilyShutoffRequested,
     );
 
-    const [durationInSec] = process.hrtime(start);
-
     const data = [...installedTransformed, ...temporarilyShutoffTransformed];
+
+    return data;
+  }
+
+
+  async calculate(gemeindeSchluessel: string, einwohnerzahl: number) {
+    const start = process.hrtime();
+    const data = await this.requestData(gemeindeSchluessel);
+    const [durationInSec] = process.hrtime(start);
 
     const ort = data[0].Ort;
     const netzbetreiberName = data[0].NetzbetreiberNamen;
+
+    const dataAtStart = this.calculateDataSetPerDate(
+      data, 
+      einwohnerzahl,
+      null,
+      // technical start date of wattbewerb
+      moment('2021-02-13T01:00:00+01:00'), 
+    )
+
+    return {
+      ort,
+      netzbetreiberName,
+      gemeindeSchluessel,
+      start: dataAtStart,
+      growth: [],
+      secs: durationInSec,
+    };
+  }
+
+
+  calculateDataSetPerDate(
+    inputData: EinheitStromerzeugungExtended[], 
+    einwohnerzahl: number, 
+    fromDate: moment.Moment | null, 
+    toDate: moment.Moment | null
+  ) {
+    let data = inputData;
+    if (fromDate) {
+      data = data.filter(
+        (entry) => 
+          (entry.InbetriebnahmeDatum.isAfter(fromDate)
+          && entry.EinheitMeldeDatum.isAfter(fromDate))
+      )
+    }
+    if (toDate) {
+      data = data.filter(
+        (entry) => 
+          (entry.InbetriebnahmeDatum.isBefore(toDate)
+          && entry.EinheitMeldeDatum.isBefore(toDate))
+      )
+    }
 
     const geprueftData = data.filter(
       (entry) =>
@@ -61,41 +110,44 @@ export class CalculatorService {
         entry.IsNBPruefungAbgeschlossen === Netzbetreiberpruefung.IN_PRUEFUNG,
     );
 
-    const totalNow = Math.floor(
-      data.reduce((memo, entry) => memo + entry.Bruttoleistung, 0),
-    );
-    const geprueftNow = Math.floor(
-      geprueftData.reduce((memo, entry) => memo + entry.Bruttoleistung, 0),
-    );
-    const inPruefungNow = Math.floor(
-      inPruefungData.reduce((memo, entry) => memo + entry.Bruttoleistung, 0),
-    );
+    const total = Math.floor(this.aggregateNumeric(data, 'Bruttoleistung'));
+    const geprueft = Math.floor(this.aggregateNumeric(geprueftData, 'Bruttoleistung'));
+    const inPruefung = Math.floor(this.aggregateNumeric(inPruefungData, 'Bruttoleistung'));
 
-    const perResidentNow = (totalNow / einwohnerZahl) * 1000;
-    const geprueftPerResidentNow = (geprueftNow / einwohnerZahl) * 1000;
-    const inPruefungPerResidentNow = (inPruefungNow / einwohnerZahl) * 1000;
+    const totalModule = Math.floor(this.aggregateNumeric(data, 'AnzahlSolarModule'));
+    const geprueftModule = Math.floor(this.aggregateNumeric(geprueftData, 'AnzahlSolarModule'));
+    const inPruefungModule = Math.floor(this.aggregateNumeric(inPruefungData, 'AnzahlSolarModule'));
+
+    const perResidentNow = (total / einwohnerzahl) * 1000;
+    const geprueftPerResidentNow = (geprueft / einwohnerzahl) * 1000;
+    const inPruefungPerResidentNow = (inPruefung / einwohnerzahl) * 1000;
 
     return {
-      ort,
-      netzbetreiberName,
-      gemeindeSchluessel,
-      now: {
-        date: moment().format('DD.MM.YYYY'),
+      dateFrom: fromDate ? fromDate.format('DD.MM.YYYY') : '',
+      dateTo: toDate ? toDate.format('DD.MM.YYYY') : '',
+      total: {
+        kwp: total,
         anlagen: data.length,
-        total: totalNow,
-        perResident: perResidentNow,
+        module: totalModule,
+        wpPerResident: perResidentNow,
       },
       geprueft: {
+        kwp: geprueft,
         anlagen: geprueftData.length,
-        total: geprueftNow,
-        perResident: geprueftPerResidentNow,
+        module: geprueftModule,
+        wpPerResident: geprueftPerResidentNow,
       },
       inPruefung: {
+        kwp: inPruefung,
         anlagen: inPruefungData.length,
-        total: inPruefungNow,
-        perResident: inPruefungPerResidentNow,
+        module: inPruefungModule,
+        wpPerResident: inPruefungPerResidentNow,
       },
-      secs: durationInSec,
-    };
+    }
+  }
+
+
+  aggregateNumeric(data: EinheitStromerzeugungExtended[], field: keyof EinheitStromerzeugungExtended) {
+    return data.reduce((memo, entry) => memo + (entry[field] as number), 0)
   }
 }
